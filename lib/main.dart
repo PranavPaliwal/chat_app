924372fdb75679674f4b2ac74ba3c56fb255e0d6
Pranav Paliwal <pranavpaliwal6@gmail.com>
2024-09-21 18:41:11 +0530

    Integrate Firebase into FlutterChat app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:chat_app/screens/auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const App());
 }
 
@@ -13,7 +19,6 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
             seedColor:const Color.fromARGB(255, 255, 20, 147),
         )
