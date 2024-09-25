73b6bafe3fec42817582034d9dcc56efcdb7762a
Pranav Paliwal <pranavpaliwal6@gmail.com>
2024-09-26 03:35:50 +0530

    Chat widget: less hot mess

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +7,35 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('No new Messages'),);
+
+    return StreamBuilder(
+      stream: FirebaseFirestore
+      .instance.collection('Chat')
+      .orderBy('createdAt',
+      descending: false)
+      .snapshots(),
+       builder: (ctx, chatSnapShots){
+        if(chatSnapShots.connectionState == ConnectionState.waiting){
+        return const Center(
+          child: CircularProgressIndicator(),
+          );
+        }
+        if(!chatSnapShots.hasData || chatSnapShots.data!.docs.isEmpty){
+          return const Center(
+          child:  Text('No new Messages'),
+          );
+        }
+
+        if(chatSnapShots.hasError){
+
+           return const Center(
+          child:  Text('Oops! Something went Wrong'),
+          );
+
+        }
+
+        final loadedMessages=chatSnapShots.data!.docs;
+        return ListView.builder(itemCount: loadedMessages.length,itemBuilder: (ctx,index)=>Text(loadedMessages[index].data()['text']),);
+       });
   }
 }
